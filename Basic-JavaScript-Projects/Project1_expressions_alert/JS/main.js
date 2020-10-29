b363dbdc068d17b7bfb3e4a8f172d955f9f0221d
Jaibean <jaimie.bertoli@gmail.com>
2020-10-28 20:52:32 -0400

    ASSIGNMENT SUBMISSION
    Project1_expressions_alert

---
+++ Basic-JavaScript-Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,19 @@
+window.alert("hello, world"); // alert message 
+
+var quote = "this is a string \"string inside a string\" end quote "; //how to make quotes visable
+document.write(quote);
+
+var con = "hi my name is";
+document.write(con + " slim shady"); 
+
+var B = "Hi my name is " + "SLIM SHADY ";
+document.write(B);
+
+var Family = "Bertoli", Dad="Mark", Mom = "Val", Daughter = "Jaimie";  //assigning mutiple varianles
+document.write(Mom);
+
+var result = B.fontcolor("red");   //adding a font color
+document.write(result);
+
+var ex = (3 + 3);
+document.write(ex);  //an expression 
